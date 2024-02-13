5e88c192669e4e568c56e018ea42d35003976f25
dorsa-nouri <dorsa.nouri23@gmail.com>
2024-02-13 11:52:10 +0330

    done

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HomeView from './components/HomeView.vue';
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <div>
+    <header>
+      <NavBar></NavBar>
+    </header>
+    <HomeView></HomeView>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
